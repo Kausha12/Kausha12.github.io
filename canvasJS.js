206cabaed9e4e5a780c8a45d9e589e44b4b3045d
Kausha12 <60319714+Kausha12@users.noreply.github.com>
2021-05-02 16:23:27 +0530

    A simple Graphic using Canvas

---
+++ canvasJS.js
@@ -0,0 +1,125 @@
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+
+let loadImage = (src, callback) => {
+    let img = document.createElement("img");
+    img.onload = () => callback(img);
+    img.src = src;
+};
+
+let imagePath = (animation, frameNumber) => {
+    return animation + "/" + frameNumber + ".png";
+};
+let frames = {
+    idle: [1, 2, 3, 4, 5, 6, 7, 8],
+    kick: [1, 2, 3, 4, 5, 6, 7],
+    punch: [1, 2, 3, 4, 5, 6, 7],
+    backward: [1, 2, 3, 4, 5, 6],
+    block: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    forward: [1, 2, 3, 4, 5, 6],
+};
+
+let loadImages = (callback) => {
+    let images = { idle: [], kick: [], punch: [], backward: [], block: [], forward: [] };
+    let ImagesToLoad = 0;
+    
+   ["idle", "kick", "punch", "block", "forward", "backward"].forEach((animation) => {
+
+       let animationframes = frames[animation];
+       ImagesToLoad = ImagesToLoad + animationframes.length;
+
+       animationframes.forEach((frameNumber) => {
+       let path = imagePath(animation, frameNumber);
+
+        loadImage(path , (image) => {
+            images[animation][frameNumber-1] = image;
+            ImagesToLoad = ImagesToLoad-1;
+ 
+            if(ImagesToLoad === 0){
+                callback(images);
+            }
+        });
+
+       });
+      
+   });
+};
+
+let animate = (ctx, images, animation, callback) => {
+    images[animation].forEach((image, index) => {
+        setTimeout(() => {
+            ctx.clearRect(0, 0, 500, 500);
+           ctx.drawImage(image, 0, 0, 500, 500); 
+        }, index*100);
+    });
+    setTimeout(callback, images[animation].length * 100);
+};
+
+loadImages((images) => {
+
+    let queuedAnimation = [];
+    
+    let aux = () => {
+        let selectedAnimation;
+
+        if(queuedAnimation.length === 0){
+            selectedAnimation = "idle";
+        }
+        else{
+            selectedAnimation = queuedAnimation.shift();
+        }
+
+        animate(ctx, images, selectedAnimation, aux);
+    };
+   aux();
+    /* 
+    let aux = () => {
+        let selectedAnimation;
+
+        if (queuedAnimation === 0){
+            selectedAnimation = "idle";
+        }
+        else{
+            selectedAnimation = queuedAnimation.shift();
+        } */
+
+document.getElementById("kick").onclick = () => {
+    queuedAnimation.push("kick");;
+};
+document.getElementById("punch").onclick = () => {
+    queuedAnimation.push("punch");
+};
+
+document.getElementById("backward").onclick = () => {
+    queuedAnimation.push("backward");
+};
+document.getElementById("block").onclick = () => {
+    queuedAnimation.push("block");
+};
+document.getElementById("forward").onclick = () => {
+    queuedAnimation.push("forward");
+};
+document.addEventListener("keyup", (Event) =>{
+    const key = Event.key;
+
+    if(key === "ArrowLeft"){
+        queuedAnimation.push("kick");
+    }
+    else if (key === "ArrowRight"){
+        queuedAnimation.push("punch");
+    }
+    else if (key === "ArrowUp"){
+        queuedAnimation.push("block");
+    }
+    else if (key === "ArrowDown"){
+        queuedAnimation.push("forward");
+    }
+    else if (key === " "){
+        queuedAnimation.push("backward");
+    }
+});
+   
+}); 
+
+
